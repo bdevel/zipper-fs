573c573733a4d7099491aa0af718001579c52fdd
Kanishk Kumar <kanishkkumar188nrt@gmail.com>
2020-08-19 09:41:37 +0530

    Added Build Node for Long and Double

--- src/zipper_fs/more_records.clj
+++ src/zipper_fs/more_records.clj
@@ -7,24 +7,34 @@
                      up-node
                      down-node])
 
+(defrecord NumericNode [current
+                        left-nodes
+                        right-nodes
+                        up-node
+                        down-node])
+
+(extend NumericNode 
+  p/NodeProtocol
+  (-> p/base-implementation
+      (assoc :down (fn [this] this))))
 (extend SeqNode
   p/NodeProtocol
   (-> p/base-implementation
       (assoc :down (fn [this]
                      (if (:down-node this)
                        (p/cursor-down this)
-                       (-> this
-                         :current
-                         :value
-                         p/build
-                         (assoc :up-node (assoc this :down-node nil))))))))
+                       (let [nodes (map p/build (get-in this [:current :value]))]
+                         (->SeqNode (:current (first nodes)) 
+                                    (list) 
+                                    (rest nodes) 
+                                    (assoc this :down-node nil)
+                                    nil)))))))
 
 (defn make-list-node
   ""
   [l]
   (println "Building" l)
-  (let [nodes (map #(SeqNode. (p/->Entity %) nil nil nil nil) l)]
-    (->SeqNode (:current (first nodes)) (list) (rest nodes) nil nil)))
+  (->SeqNode (p/->Entity l) nil nil nil nil))
 
 
 (extend-protocol p/NodeProtocol
@@ -35,26 +45,32 @@
   java.lang.String
   (build [this] (make-list-node (seq this)))
   clojure.lang.PersistentHashSet
-  (build [this] (make-list-node (seq this))))
+  (build [this] (make-list-node (seq this)))
+  java.lang.Long
+  (build [this] (make-list-node this))
+  java.lang.Double
+  (build [this] (make-list-node this)))
 
 
 
 (comment
   
   (-> (p/build '(1 2 (3.0 3.1) [4.0, 4.1] "hey you" #{"setv1" "setv2"}))
-   
-      p/right
+      ;;(p/build-insert-left "test")
+      ;;p/right
       ;;(assoc :left-nodes nil)
-      p/right
-      p/right
+      ;;p/right
+      ;;p/right
       p/down
       p/right
-      p/up
-      ;;p/down
+      p/right
+      ;;p/up
+      
+      p/down
       ;;:up-node
       ;;p/up
       ;; p/left
-      ;; p/left
+      ;;p/left
       ;; p/down
       ;; p/right
       ;; p/up
